88158537fef4bb6118b20591d2a838e012bb99cf
Mustafa Koçer <mkocerim@gmail.com>
2023-03-20 22:45:47 +0300

    activity added

--- src/components/bodySection/activitySection/Activity.jsx
+++ src/components/bodySection/activitySection/Activity.jsx
@@ -1,7 +1,71 @@
 import React from "react";
 import "./activity.css";
+
+//imported icons===>>>>
+import { BsArrowRightShort } from "react-icons/bs";
+
+//imported Images====>>
+import user from "../../../Assets/images/user2.jpg";
+import user2 from "../../../Assets/images/user3.jpg";
+import user3 from "../../../Assets/images/user4.jpg";
+import user4 from "../../../Assets/images/user5.jpg";
+import user5 from "../../../Assets/images/user2.jpg";
+
 const Activity = () => {
-  return <div>ACTIVITY</div>;
+  return (
+    <div className="activitySection">
+      <div className="heading flex">
+        <h1>Resent Activity</h1>
+        <button className="btn flex">
+          see All
+          <BsArrowRightShort className="icon" />
+        </button>
+      </div>
+
+      <div className="secContainer grid">
+        <div className="singleCustomer flex">
+          <img src={user} alt="Customer Image" className="imgCustomer" />
+          <div className="customerDetails">
+            <span className="name">Ola Martha</span>
+            <small> Ordered a new plant</small>
+          </div>
+          <div className="duration"> 2 min ago</div>
+        </div>
+        <div className="singleCustomer flex">
+          <img src={user2} alt="Customer Image" className="imgCustomer" />
+          <div className="customerDetails">
+            <span className="name">Ola Martha</span>
+            <small> Ordered a new plant</small>
+          </div>
+          <div className="duration"> 2 min ago</div>
+        </div>
+        <div className="singleCustomer flex">
+          <img src={user3} alt="Customer Image" className="imgCustomer" />
+          <div className="customerDetails">
+            <span className="name">Ola Martha</span>
+            <small> Ordered a new plant</small>
+          </div>
+          <div className="duration"> 2 min ago</div>
+        </div>
+        <div className="singleCustomer flex">
+          <img src={user4} alt="Customer Image" className="imgCustomer" />
+          <div className="customerDetails">
+            <span className="name">Ola Martha</span>
+            <small> Ordered a new plant</small>
+          </div>
+          <div className="duration"> 2 min ago</div>
+        </div>
+        <div className="singleCustomer flex">
+          <img src={user5} alt="Customer Image" className="imgCustomer" />
+          <div className="customerDetails">
+            <span className="name">Ola Martha</span>
+            <small> Ordered a new plant</small>
+          </div>
+          <div className="duration"> 2 min ago</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Activity;
--- src/components/bodySection/listingSection/Listing.jsx
+++ src/components/bodySection/listingSection/Listing.jsx
@@ -4,9 +4,17 @@ import "./listing.css";
 //imported Icons=====>>
 import { BsArrowRightShort } from "react-icons/bs";
 import { AiFillHeart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
 
 //imported Images=====>>>
-import img3 from "../../../Assets/images/image3.jpg";
+import img1 from "../../../Assets/images/image5.jpg";
+import img2 from "../../../Assets/images/image19.jpg";
+import img3 from "../../../Assets/images/image16.jpg";
+import img4 from "../../../Assets/images/image14.jpg";
+import usr1 from "../../../Assets/images/user2.jpg";
+import usr2 from "../../../Assets/images/user3.jpg";
+import usr3 from "../../../Assets/images/user4.jpg";
+import usr4 from "../../../Assets/images/user5.jpg";
 
 const Listing = () => {
   return (
@@ -20,8 +28,77 @@ const Listing = () => {
       <div className="secContainer flex">
         <div className="singleItem">
           <AiFillHeart className="icon" />
+          <img src={img1} alt="Image Name" />
+          <h3>Annual Vince</h3>
+        </div>
+
+        <div className="singleItem ">
+          <AiOutlineHeart className="icon" />
+          <img src={img2} alt="Image Name" />
+          <h3>Coffe Plant</h3>
+        </div>
+
+        <div className="singleItem">
+          <AiOutlineHeart className="icon" />
           <img src={img3} alt="Image Name" />
-          <h1>Annual Vince</h1>
+          <h3>Button Fern</h3>
+        </div>
+
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <img src={img4} alt="Image Name" />
+          <h3>Spider Plant</h3>
+        </div>
+      </div>
+
+      <div className="sellers flex">
+        <div className="topSellers">
+          <div className="heading flex">
+            <h3>Top Sellers</h3>
+            <button className="btn flex">
+              See All <BsArrowRightShort className="icon" />
+            </button>
+          </div>
+          <div className="card flex">
+            <div className="users">
+              <img src={usr1} alt="User Image" />
+              <img src={usr2} alt="User Image" />
+              <img src={usr3} alt="User Image" />
+              <img src={usr4} alt="User Image" />
+            </div>
+            <div className="cardText">
+              <span>
+                11.257 plant sold <br />{" "}
+                <small>
+                  72 Sellers <span className="date">7 Days</span>
+                </small>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="featuredSellers">
+          <div className="heading flex">
+            <h3>Featured Sellers</h3>
+            <button className="btn flex">
+              See All <BsArrowRightShort className="icon" />
+            </button>
+          </div>
+          <div className="card flex">
+            <div className="users">
+              <img src={usr4} alt="User Image" />
+              <img src={usr2} alt="User Image" />
+              <img src={usr1} alt="User Image" />
+              <img src={usr3} alt="User Image" />
+            </div>
+            <div className="cardText">
+              <span>
+                23,257 plant sold <br />
+                <small>
+                  28 Sellers <span className="date">27 Days</span>
+                </small>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
